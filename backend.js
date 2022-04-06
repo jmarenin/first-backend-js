43d44d42f75785297386f098022a85f89b69dadd
unknown <jmarenin@calpoly.edu>
2022-04-06 00:44:01 -0700

    ID identifier

--- backend.js
+++ backend.js
@@ -54,7 +54,20 @@ app.get('/users', (req, res) => {
         res.send(users);
     }
 });
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
 
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id);
+}
 
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}');
